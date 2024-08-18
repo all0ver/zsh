144cb68b1bf8bfb407b4c4597cbb95f7ed245b0f
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-18 20:08:27 +0200

    add wezterm opacity

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,6 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.window_decorations = "NONE"
+config.window_background_opacity = 0.8
 -- For example, changing the color scheme:
 config.color_scheme = 'Flatland'
 
